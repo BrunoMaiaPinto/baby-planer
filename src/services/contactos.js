34f41327c579227a6fa960b2ef660f93eb73ac4f
BrunoMaiaPinto <brunommpinto@sapo.pt>
2026-07-03 17:22:54 +0100

    fix: update fetch URL in getData function to correct endpoint

--- src/services/contactos.js
+++ src/services/contactos.js
@@ -1,6 +1,6 @@
 export const getData = async function () {
   const res = await fetch(
-    "https://gist.githubusercontent.com/BrunoMaiaPinto/efa10d7df693b46ce3dc247f577d8e82/raw/99aa633cc1f8a314706f2987d1fc4abe8f9f7de3/alimenta%25C3%25A7%25C3%25A3o.json",
+    "https://gist.githubusercontent.com/BrunoMaiaPinto/efa10d7df693b46ce3dc247f577d8e82/raw/67586eb1b63979c474557936da3e9f18f970e840/contactos.json",
   );
   const data = await res.json();
 
